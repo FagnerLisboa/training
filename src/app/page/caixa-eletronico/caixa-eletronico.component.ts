9028fa359241373a5d3495222a8b7c9862b17494
Fagner Lisboa <fnlisboa2016@gmail.com>
2022-11-03 10:00:59 -0300

    caixa eletrônico

--- src/app/page/caixa-eletronico/caixa-eletronico.component.ts
+++ src/app/page/caixa-eletronico/caixa-eletronico.component.ts
@@ -6,52 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./caixa-eletronico.component.css']
 })
 export class CaixaEletronicoComponent implements OnInit {
-  public saldo!: number;
-  public saque!: number;
-  public deposito!: string;
-  public valor!: string;
-  sair!: string;
+  valor: number;
+  saldo = 50;
+  sair: boolean;
+  showSaldo: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
-
+    this.showSaldo = false; 0
   }
+  consultarSaldo() {
+    this.showSaldo = true;
+  };
 
-  consultarSaldo({ saldo }: { saldo: 0.00; }) {
-    
-    this.valor = (` saldo atual é de R$${0.00}`);
-    
+  realizarDeposito() {
+    this.saldo += this.valor;
+  };
+
+  realizarSaque() {
+    if (this.saldo = this.saldo) {
+
+      this.saldo -= this.valor
+
+      this.valor <= this.saldo;
+
+      console.log("ok");
+
+    } else {
+      this.saldo <= 0;
+
+      console.log("SEM SALDO");
     }
-
-  realizarDeposito(){
-    this.saldo += parseFloat(this.deposito);
-    alert('Deposito realizado com sucesso!');
-    return this.saldo;
-  }
-
-  realizarSaque(){
-    let saque = this.realizarSaque;
-    this.saque = parseFloat(`saque`);
-
-    if (this.saque > this.saldo) {
-      alert('Não é possível realizar o saque');
-      return this.saldo;
-    }
-    
-    this.saldo -= this.saque;
-    alert('Saque realizado com sucesso!');
-    return this.saldo;
-  }
-
-  onClickSaldo(){
-    console.log(this.consultarSaldo);
-    console.log(this.realizarDeposito);
-    console.log(this.realizarSaque);
-    
-    
-    
-
-  }
- 
+  };
 }
--- src/app/page/lampada/lampada.component.ts
+++ src/app/page/lampada/lampada.component.ts
@@ -1,85 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-lampada',
   templateUrl: './lampada.component.html',
   styleUrls: ['./lampada.component.css']
 })
 export class LampadaComponent implements OnInit {
+  lampada: string;
+  isLigada: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
-    var atual_state = 'DESLIGADA';
-
-    function mudaEstado() {
-      atual_state === 'DESLIGADA' ? atual_state = 'LIGADA' : atual_state = 'DESLIGADA';
-      return atual_state;
-    }
-
-    function ligaDesliga() {
-
-      if (atual_state === 'DESLIGADA')
-        (<HTMLImageElement>document.getElementById("lampada")).src = 'assets/ligada.jpg';
-      else
-        (<HTMLImageElement>document.getElementById("lampada")).src = 'assets/desligada.jpg';
-
-      mudaEstado();
-    }
-
-    (<HTMLImageElement>document.getElementById("lampada")).addEventListener("click", ligaDesliga);
-
-
-
-  
-
-
+    this.lampada = 'assets/desligada.jpg';
+    this.isLigada = true;
   }
-  
-    
-  ligaDesliga(){
-    var lampada = 'DESLIGADA';
 
-      if (lampada === 'DESLIGADA'){
-        lampada = 'assets/desligada.jpg'
-        
-      }else {
-        lampada = 'assets/ligada.jpg'
-      }
-      
+  ligaDesliga() {
+    if (this.isLigada === true) {
+      this.lampada = 'assets/desligada.jpg';
+      this.isLigada = false;
+    } else {
+      this.lampada = 'assets/ligada.jpg';
+      this.isLigada = true;
+    }
   }
-  
-
-    
-    
-
-
-
-
-    
-
-    // var atual_state = 'DESLIGADA';
-
-    // function mudaEstado() {
-    //   atual_state === 'DESLIGADA' ? atual_state = 'LIGADA' : atual_state = 'DESLIGADA';
-    //   return atual_state;
-    // }
-
-    // function ligaDesliga() {
-
-    //   if (atual_state === 'DESLIGADA')
-    //     (<HTMLImageElement>document.getElementById("lampada")).src = 'assets/ligada.jpg';
-    //   else
-    //     (<HTMLImageElement>document.getElementById("lampada")).src = 'assets/desligada.jpg';
-
-    //   mudaEstado();
-    // }
-
-    // (<HTMLImageElement>document.getElementById("lampada")).addEventListener("click", ligaDesliga);
-
-
-
-  
-
 }
+
+
+//*********************************************OUTRO METODO*************************************************/
+
+  // var atual_state = 'DESLIGADA';
+  // function mudaEstado() {
+  //   atual_state === 'DESLIGADA' ? atual_state = 'LIGADA' : atual_state = 'DESLIGADA';
+  //   return atual_state;
+  // }
+  // function ligaDesliga() {
+  //   if (atual_state === 'DESLIGADA')
+  //     (<HTMLImageElement>document.getElementById("lampada")).src = 'assets/ligada.jpg';
+  //   else
+  //     (<HTMLImageElement>document.getElementById("lampada")).src = 'assets/desligada.jpg';
+  //   mudaEstado();
+  // }
+  // (<HTMLImageElement>document.getElementById("lampada")).addEventListener("click", ligaDesliga);
